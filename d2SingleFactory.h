0a81d7005ebd3a56ecd9458c3c6aa612a2fa6c81
fU9ANg <bb.newlife@gmail.com>
2013-04-14 05:34:42 +0800

    add comment for SINGLEFACTORY class

--- d2SingleFactory.h
+++ d2SingleFactory.h
@@ -12,6 +12,7 @@
 #include "d2MemZone.h"
 #include "d2MemBlock.h"
 
+// only instance (global)
 class d2SingleFactory
 {
     public:
@@ -19,15 +20,21 @@ class d2SingleFactory
         static class d2SingleFactory* instance();
 
     public:
+        // receive queue (evLoop ----> FTPTask)
         D2TASKQUEUE m_recvQueue;
+        // send queue (FTPTask ----> SendTask)
         D2TASKQUEUE m_sendQueue;
+        // memory-manager
         d2MemZone <d2MemBlock > m_memZone;
 
+        // receive data handler
         static int recvData (int fd, void* buffer, unsigned int len);
+        // send data handler
         static int sendData (int fd, void* buffer, unsigned int len);
 
     private:
         d2SingleFactory ();
+        // single pattern
         static class d2SingleFactory* m_staticObject;
 };
 
